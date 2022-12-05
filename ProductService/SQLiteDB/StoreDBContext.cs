fd5320e9b23e7adb54083ecda020f26c9356d80f
Matt Yang <Matt.Yang@arris.com>
2022-12-06 07:17:52 +0800

    DB creation set up the storecontext and connectionstring

---
+++ ProductService/SQLiteDB/StoreDBContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ProductService.Commands;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ProductService.SQLiteDB
+{
+    public class StoreDBContext:DbContext
+    {
+        public StoreDBContext():base()
+        {}
+
+        public DbSet<NewProductCommand> Products { get; set; }
+    }
+}
