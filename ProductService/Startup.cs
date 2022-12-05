b2010385868d55a25fe26bc84dad622626a222ce
Matt Yang <Matt.Yang@arris.com>
2022-12-06 06:32:24 +0800

    pre db creation commit

--- ProductService/Startup.cs
+++ ProductService/Startup.cs
@@ -43,7 +43,6 @@ namespace ProductService
             //    options.MinimumSameSitePolicy = SameSiteMode.None;
             //});
 
-            services.
             services.AddConsul();
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
             services.AddCors(options =>
