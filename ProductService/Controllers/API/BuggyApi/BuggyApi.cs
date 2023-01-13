f05a4e777280f90d5207198043ac16d4d633c103
mattcoder2017 <myang@arris.com>
2023-01-13 16:18:13 +0800

    Add buggy webapi

---
+++ ProductService/Controllers/API/BuggyApi/BuggyApi.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ProductService.Controllers.API.BuggyApi
+{
+    [Route("api/buggy")]
+    public class BuggyApi: ControllerBase
+    {
+        [HttpGet("servererror")]
+        public ActionResult GetServerError()
+        {
+            int[] errorArray = new int[2] { 1, 2 };
+            String teststring = "matt";
+            int errorVar = int.Parse(teststring);
+            return Ok();
+        }
+
+        [HttpGet("autherror")]
+        [Authorize]
+        public ActionResult GetAuthError()
+        {
+            return Ok();
+        }
+    }
+}
