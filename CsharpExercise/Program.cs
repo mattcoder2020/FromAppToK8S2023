7feb4b22de378a8e12a9a23dd51295aeed5c5bb4
mattcoder2017 <myang@arris.com>
2023-02-13 10:53:25 +0800

    charpexercise-add ReverseString and Find2ndLargestNum function

--- CsharpExercise/Program.cs
+++ CsharpExercise/Program.cs
@@ -8,8 +8,8 @@ namespace CsharpExercise
     {
         static void Main(string[] args)
         {
-            SortString(new string[] { "aacd", "bbcd", "aaac", "abc"});
-            SortIntegers(new int[] {4,3,1,2 });
+            SortString(new string[] { "aacd", "bbcd", "aaac", "abc" });
+            SortIntegers(new int[] { 4, 3, 1, 2 });
             int[] Fibonics = FindFibonics(10);
             foreach (int i in Fibonics)
             {
@@ -23,11 +23,13 @@ namespace CsharpExercise
             }
 
             Console.WriteLine(solution("red2 blue5 black4 green1 gold3"));
-
+            Console.WriteLine(solution1("red2 blue5 black4 green1 gold3"));
+            Console.WriteLine("Reverse string program ->" + ReverseString("program"));
+            Console.WriteLine("Second largest number among [4,2,6,12,1,3] is " + FindSecondLargest(new int[] { 4, 2, 6, 12, 1, 3 }));
             //Delegate
             PhotoLoader l = new PhotoLoader(new Photo());
             PhotoHandler h = new PhotoHandler();
-              //assigning 3 methods to delegate object
+
             PhotoLoader.photohandling h1 = h.AdjustEffect;
             h1 += h.AdjustSaturation;
             h1 += h.AdjustTransparency;
@@ -48,18 +50,19 @@ namespace CsharpExercise
             Event.Controller.SubscribeToPub();
 
         }
-
+        //   Input: 10
+        //   Output: 0 1 1 2 3 5 8 13 21 34 55
         private static int[] FindFibonics(int v)
         {
             // Fibonic numbers 0, 1, 1, 2, 3, 5, 8, 13
-                int first = 0;
-                int second = 1;
-            int[] fibonics = new int[v+1];
+            int first = 0;
+            int second = 1;
+            int[] fibonics = new int[v + 1];
             int index = 0;
             int sum = 0;
             fibonics[0] = first;
             fibonics[1] = second;
-            for (int i=2; i<=v ; i++ )
+            for (int i = 2; i <= v; i++)
             {
                 sum = first + second;
                 fibonics[i] = sum;
@@ -70,6 +73,34 @@ namespace CsharpExercise
 
         }
 
+
+        //input 4,2,6,12,1,3
+        //output 6
+        private static int FindSecondLargest(int[] input)
+        {
+            int max = 0, temp = 0;
+            foreach (int i in input)
+            {
+                if (i > temp)
+                {
+                    temp = i;
+                }
+            }
+            max = temp;
+            temp = 0;
+            foreach (int i in input)
+            {
+                if (i > temp && i != max)
+                {
+                    temp = i;
+
+                }
+            }
+            return temp;
+
+        }
+        //   Input: str = 10
+        //   Output: 2 3 5 7
         private static int[] FindPrimeNumber(int v)
         {
             int[] primenumber = new int[v];
@@ -80,13 +111,14 @@ namespace CsharpExercise
                 isPrime = true;
                 for (int j = 2; j <= v; j++)
                 {
-                    if (i != j && i%j ==0 )
+                    if (i != j && i % j == 0)
                     {
                         isPrime = false;
-                        break; }
+                        break;
+                    }
 
                 }
-                if(isPrime)
+                if (isPrime)
                 {
                     primenumber[matchcount] = i;
                     matchcount++;
@@ -99,7 +131,7 @@ namespace CsharpExercise
         }
 
         //   Input: str = "red2 blue5 black4 green1 gold3"
-        //    /Output: "green red gold black blue"
+        //   Output: "green red gold black blue"
         public static string solution(string s)
         {
             string[] str = s.Split(" ");
@@ -123,6 +155,35 @@ namespace CsharpExercise
 
         }
 
+        //   Input: str = "red2 blue5 black4 green1 gold3"
+        //   Output: "green red gold black blue"
+        public static string solution1(string s)
+        {
+            string[] str = s.Split(" ");
+            string returnstr = string.Empty;
+
+            for (int i = 1; i <= str.Length; i++)
+            {
+                foreach (string s1 in str)
+                {
+                    if (int.Parse(s1.Substring(s1.Length - 1)) == i)
+                        returnstr += s1.Substring(0, s1.Length - 1) + " ";
+                }
+            }
+            return returnstr.Substring(0, returnstr.Length - 1);
+
+        }
+
+        public static string ReverseString(string input)
+        {
+            int i = input.Length;
+            string returnstring = string.Empty;
+            for (int u = i; u > 0; u--)
+            {
+                returnstring += input.Substring(u - 1, 1);
+            }
+            return returnstring;
+        }
         public static void SortString(string[] input)
         {
             // Console.WriteLine((int)input[0].ToCharArray()[0]);
@@ -156,9 +217,9 @@ namespace CsharpExercise
                 int chr = -1;
                 for (int i = 0; i < 1000; i++)
                 {
-                    if (input[j].Length > i 
-                        &&(int)input[j].ToCharArray()[i] > 0 
-                        && (int)input[j].ToCharArray()[i] > chr 
+                    if (input[j].Length > i
+                        && (int)input[j].ToCharArray()[i] > 0
+                        && (int)input[j].ToCharArray()[i] > chr
                         && input[j] != null)
                     {
                         pos = j;
@@ -168,26 +229,26 @@ namespace CsharpExercise
                 input[pos] = null;
 
             }
-            string str; 
+            string str;
             for (int i = 0; i < output.Length; i++)
             {
-                str = output[i] ;
+                str = output[i];
                 Console.WriteLine(str);
             }
 
-            
+
         }
 
-           
-        
+
+
 
         public static int[] SortIntegers(int[] input)
         {
             Console.WriteLine("Before Sorting");
             printArray(input);
-            for (int i = 0; i<input.Length; i++)
+            for (int i = 0; i < input.Length; i++)
             {
-                for (int w = 0; w< input.Length; w++)
+                for (int w = 0; w < input.Length; w++)
                 {
                     if (input[i] > input[w])
                         swap(input, i, w);
@@ -204,7 +265,7 @@ namespace CsharpExercise
 
         private static void printArray(int[] input)
         {
-            String str =String.Empty;
+            String str = String.Empty;
             for (int i = 0; i < input.Length; i++)
             {
                 str += input[i] + " ";
@@ -213,15 +274,15 @@ namespace CsharpExercise
             Console.WriteLine(str);
         }
 
-        private static void swap<T> (T[] input, int old_e, int new_e)
-            {
-                 T temp ;
+        private static void swap<T>(T[] input, int old_e, int new_e)
+        {
+            T temp;
             temp = input[old_e];
             input[old_e] = input[new_e];
             input[new_e] = temp;
 
-            }
-        
+        }
+
 
 
     }
