1c6cd8323b6b760b735535db1af6c92e68a13621
mattcoder2020 <mattcoder2013@yahoo.com>
2023-02-27 08:05:00 +0800

    WebUI- Added navigtation enclosed error for routing to be rendered in exception view UI

--- AngularNetCoreProj/ClientApp/src/app/coremodule/error/exceptionview/exceptionview.component.ts
+++ AngularNetCoreProj/ClientApp/src/app/coremodule/error/exceptionview/exceptionview.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-exceptionview',
@@ -6,5 +7,10 @@ import { Component } from '@angular/core';
   styleUrls: ['./exceptionview.component.scss']
 })
 export class ExceptionviewComponent {
+  error: any;
 
+  constructor(private router: Router) {
+    const navigation = this.router.getCurrentNavigation();
+    this.error = navigation?.extras?.state?.['error'];
+  }
 }
