72b3c77b19a67587b0489e7596630cc82361ecac
mattcoder2020 <mattcoder2013@yahoo.com>
2023-06-10 09:47:33 +0800

    change

--- InventoryService/Events/InventoryUpdated.cs
+++ InventoryService/Events/InventoryUpdated.cs
@@ -7,7 +7,7 @@ namespace InventoryService.Events
     [MessageNamespace("Matt-Inventory")]
     public class InventoryUpdated : IEvent
     {
-        public int Id { get; set; }
+        public int ProductId { get; set; }
         public int Quantity { get; set; }
         public ICorrelationContext Context { get; set; }
     }
