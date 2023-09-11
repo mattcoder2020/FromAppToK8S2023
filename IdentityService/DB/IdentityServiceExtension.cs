bec513db6fe28ff5799acb287a3dfb4a17ff6ac9
Matt Yang <matt.yang@commscope.com>
2023-09-12 06:54:21 +0800

    IdentityService-part1

---
+++ IdentityService/DB/IdentityServiceExtension.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IdentityService.DB
+{
+    public static class IdentityServiceExtension
+    {
+        public static IServiceCollection AddIdentityService
+            (this IServiceCollection services, IConfiguration config)
+        {
+            services.AddDbContext<AppIdentityDbContext>(opt =>
+            opt.UseSqlite(config.GetConnectionString("identityconnection")));
+            return services;
+        }
+    }
+}
---
+++ IdentityService/Entity/Address.cs
@@ -0,0 +1,12 @@
+﻿namespace IdentityService.Entity
+{
+    public class Address
+    {
+        public Address() { }
+        public string State { get; set; }
+        public string City { get; set; }
+        public string PostalCode { get; set; }
+        public string DetailAddress { get; set; }
+        public string AppUserId { get; set; }
+           }
+}
